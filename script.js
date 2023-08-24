eac905dfddeac6c546222bbe8b254432b1ebbb75
Ahmed Karara <110180686+Ahmed-Karara@users.noreply.github.com>
2023-08-25 00:58:27 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function dropdown() {
-	var body = document.querySelector("body");
-	var menu = document.getElementById("menu");
-	if (menu.style.transform == "scaleY(1)") {
-		menu.style.transform = "scaleY(0)";
-		menu.style.opacity = "0";
-	} else {
-		menu.style.transform = "scaleY(1)";
-		menu.style.transition = "all 0.5s";
-		menu.style.opacity = "1";
-	}
+  var menu = document.getElementById("menu");
+  if (menu.style.left == "0%") {
+    menu.style.left = "-50%";
+  } else {
+    menu.style.left = "0%";
+    menu.style.transition = "all 0.5s";
+  }
 }
 
 function dropmenu() {
-	var body = document.querySelector("body");
-	var menu = document.getElementById("menu");
-	if (body.style.width <= "880px") {
-		dropdown();
-	}
-	if (body.style.width > "880px" && menu.style.transform == "scaleY(0)") {
-		menu.style.transform = "scaleY(1)";
-		menu.style.opacity = "1";
-	}
+  var body = document.querySelector("body");
+  var menu = document.getElementById("menu");
+  if (body.style.width <= "880px") {
+    dropdown();
+  }
+  if (body.style.width > "880px" && menu.style.transform == "scaleY(0)") {
+    menu.style.transform = "scaleY(1)";
+    menu.style.opacity = "1";
+  }
 }
